86b33d959ba4318ee8178f42ea77ba650836bc20
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-07-03 17:19:34 +0100

    Clearing the Form Spinner

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StatusBar } from "expo-status-bar";
+ import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { View } from "react-native";
 import { Header } from "./src/components/common";
 import LoginForm from "./src/components/LoginForm";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-export default function App() {
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyC2-Z7K-4ZEJ1E7EysXhjyN-dU1VTC_CIc",
+    authDomain: "auth-cfecb.firebaseapp.com",
+    databaseURL:
+      "https://auth-cfecb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "auth-cfecb",
+    storageBucket: "auth-cfecb.appspot.com",
+    messagingSenderId: "1035625868022",
+    appId: "1:1035625868022:web:ec47312d5488344d97dad7",
+    measurementId: "G-7M5FLN52QF",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
   return (
     <View>
       <Header headerText="Authentication" />
-      <LoginForm />
-       <StatusBar style="auto" />
+      <LoginForm auth={auth} />
+      <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
+
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,37 +1,21 @@
 import React, { useState } from "react";
-import { initializeApp } from "firebase/app";
 import {
-  getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { Button, Card, CardSection, Input, Spinner } from "./common";
 import { Text, StyleSheet } from "react-native";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC2-Z7K-4ZEJ1E7EysXhjyN-dU1VTC_CIc",
-  authDomain: "auth-cfecb.firebaseapp.com",
-  databaseURL:
-    "https://auth-cfecb-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "auth-cfecb",
-  storageBucket: "auth-cfecb.appspot.com",
-  messagingSenderId: "1035625868022",
-  appId: "1:1035625868022:web:ec47312d5488344d97dad7",
-  measurementId: "G-7M5FLN52QF",
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const LoginForm = () => {
+const LoginForm = ({ auth }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const onButtonPress = () => {
-    setLoading(true); // Set loading to true before the asynchronous operation
-    signInWithEmailAndPassword(auth, email, password)
+    setLoading(true);
+      signInWithEmailAndPassword(auth, email, password)
+        .then(onLoginSucces)
       .catch(() => {
         createUserWithEmailAndPassword(auth, email, password)
           .then(() => {
@@ -41,25 +25,26 @@ const LoginForm = () => {
             setError(error.message);
           })
           .finally(() => {
-            setLoading(false); // Set loading to false after the operation is completed
+            setLoading(false);
           });
       })
       .finally(() => {
-        setLoading(false); // Set loading to false after an error occurs
+        setLoading(false);
       });
   };
+    const onLoginSucces = () => {
+        setEmail('')
+        setPassword('')
+        setLoading(false)
+        setError('')
+    }
 
-
-
-
-    
-    const renderButton = () => {
-      if (loading) {
-        return <Spinner size="small" />;
-      }
-      return <Button onPress={onButtonPress}>Log In</Button>;
-    };
-
+  const renderButton = () => {
+    if (loading) {
+      return <Spinner size="small" />;
+    }
+    return <Button onPress={onButtonPress}>Log In</Button>;
+  };
 
   return (
     <Card>
@@ -80,9 +65,7 @@ const LoginForm = () => {
           onChangeText={(text) => setPassword(text)}
         />
       </CardSection>
-          <CardSection>
-              {renderButton()}
-      </CardSection>
+      <CardSection>{renderButton()}</CardSection>
       {error ? <Text style={styles.errorTextStyle}>{error}</Text> : null}
     </Card>
   );
